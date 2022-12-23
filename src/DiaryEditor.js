aa738691d697e0c8bf6db82556dd17c14a8e3273
Minho Kim <music4537@naver.com>
2022-12-23 16:27:59 -0600

    Feat : Diary writing name and content function added

---
+++ src/DiaryEditor.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const DiaryEditor = () => {
+
+    const [author, setAuthor] = useState("");
+    const [content, setContent] = useState("");
+
+    const [state, setState] = useState({
+        author: "",
+        content: "",
+    });
+
+    const handleChange = (e) => {
+        setState({
+            ...state,
+            [e.target.name] : e.target.value,
+        });
+        console.log(state.author);
+        console.log(state.content);
+    };
+
+    return (
+        <div className="DiaryEditor">
+            <h2>오늘의 일기</h2>
+            <div>
+                <input name="author" value={state.author} onChange={handleChange} />
+            </div>
+            
+            <div>
+                <textarea name="content" value={state.content} onChange={handleChange}/>
+            </div>
+        </div>
+    );
+}
+
+export default DiaryEditor;
